f2d19c0f261ad320d6f33a8bd6551261b4fe8509
FemkeG1996 <73695215+FemkeG1996@users.noreply.github.com>
2021-02-18 15:03:17 +0100

    frituursnacks

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
+var cors = require('cors');
 const port = process.env.PORT || 80
 
 const { Pool, Client } = require('pg')
@@ -15,10 +17,20 @@ const pool = new Pool({
     },
 })
 
+app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
 
 app.get('/lijst', (req, res) => {
 
@@ -28,6 +40,28 @@ app.get('/lijst', (req, res) => {
  })
 })
 
+app.get('/frituursnacks', (req, res) => {
+
+    pool.query('SELECT * FROM frituursnacks', (err, res2) => {
+        console.log(res2);
+        res.send(JSON.stringify(res2.rows));
+ })
+})
+
 app.listen(port, () => {
     console.log(`App Server luistert op poort ${port}`)
 })
+
+app.post('/frituursnacks/nieuw', (req, res3) =>{
+  console.log(req.body);
+  let naam = req.body.naam;
+  let tijd = req.body.frituurtijd;
+  let veg = req.body.vegetarisch;
+  pool.query('INSERT INTO frituursnacks(naam,frituurtijd,vegetarisch) VALUES ($1, $2, $3)',[ naam, tijd, veg], (error, results) => {
+    if (error) {
+      throw error
+    }
+    })
+    res3.send("stop")
+})
+    
