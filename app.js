6d835a38c66c4d7f46b2a97094b3e4ec90953127
FemkeG1996 <73695215+FemkeG1996@users.noreply.github.com>
2021-02-18 10:30:09 +0100

    Hiroku json notatie

--- app.js
+++ app.js
@@ -2,14 +2,30 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 80
 
+const { Pool, Client } = require('pg')
+const pool = new Pool({
+    user: 'rmnqaevuviknpb',
+    host: 'ec2-54-72-155-238.eu-west-1.compute.amazonaws.com',
+    database: 'd31knv4ujdglor',
+    password: '80c26307d9a840b7a7cecd4faa43a585a6265e5e10fcf4d31f3863c9eedfbb70',
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false,
+
+    },
+})
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/test', (req, res) => {
-    let arr = ['wesley', 'laura', 'brent'];
-    res.send(JSON.stringify(arr));
+app.get('/lijst', (req, res) => {
 
+    pool.query('SELECT * FROM LIJST', (err, res2) => {
+        console.log(res2);
+        res.send(JSON.stringify(res2.rows));
+ })
 })
 
 app.listen(port, () => {
